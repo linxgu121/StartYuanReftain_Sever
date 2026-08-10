3457378e579e0f2f7a6f3baf531addf562dd9f38
gu sili <11837211+wszzds@user.noreply.gitee.com>
2026-08-10 17:18:20 +0800

    实体类用户

---
+++ src/main/java/cn/niuma/lingdi000721/startyuanreftain/StartYuanReftainApplication.java
@@ -0,0 +1,13 @@
+package cn.niuma.lingdi000721.startyuanreftain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartYuanReftainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartYuanReftainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/niuma/lingdi000721/startyuanreftain/StartYuanReftainApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.niuma.lingdi000721.startyuanreftain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartYuanReftainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
